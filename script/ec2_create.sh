3970225c00530fbdbc6c149a0112b615231c3256
Aparna-Tathod <55685454+Aparna-Tathod@users.noreply.github.com>
2021-03-31 16:35:47 +0530

    Update ec2_create.sh
    indentation changes

--- script/ec2_create.sh
+++ script/ec2_create.sh
@@ -35,16 +35,16 @@ echo " EC2_CF_TEMPLATE_FILE is $EC2_CF_TEMPLATE_FILE"
 echo " Start CF Deployment of EC2 Resources on $(date +'%Y-%M-%d %T %Z') with Stack Name as ${EC2_CF_STACK_NAME}"
 
 aws cloudformation deploy \
-    --region ${AWS_REGION} \
-    --stack-name ${EC2_CF_STACK_NAME} \
-    --template-file ${EC2_CF_TEMPLATE_FILE} \
-    --parameter-overrides \
-        KeyName=${EC2_KEY_NAME} \
-        InstanceType=${EC2_INSTANCE_TYPE} \
-        EnvironmentName=${ENVIRONMENT_NAME} \
-        VpcId=${VPC_ID} \
-        SubnetId=${SUBNET_ID} \
-    --no-fail-on-empty-changeset \
-    --capabilities CAPABILITY_NAMED_IAM    
+  --region ${AWS_REGION} \
+  --stack-name ${EC2_CF_STACK_NAME} \
+  --template-file ${EC2_CF_TEMPLATE_FILE} \
+  --parameter-overrides \
+      KeyName=${EC2_KEY_NAME} \
+      InstanceType=${EC2_INSTANCE_TYPE} \
+      EnvironmentName=${ENVIRONMENT_NAME} \
+      VpcId=${VPC_ID} \
+      SubnetId=${SUBNET_ID} \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM    
 
 echo " Completed - CF Deployment of EC2 Resources on $(date + '%Y-%M-%d %T %Z' with Stack Nam as ${EC2_CF_STACK_NAME})
